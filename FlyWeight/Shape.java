ae398847f113ee76944dd2e278d7c35f93b15156
Mustafa <mostafa.labib65@gmail.com>
2017-12-11 00:18:12 +0200

    FlyWeight

---
+++ FlyWeight/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FlyWeight;
+
+/**
+ *
+ * @author hp
+ */
+public interface Shape {
+
+    /* redraw the shape on the canvas */
+    public void draw(java.awt.Graphics canvas);
+
+}
